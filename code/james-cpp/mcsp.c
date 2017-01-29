cbbfa3e40d8cc37d1d174c4cb140663ec5474c6b
James Trimble <james.trimble@yahoo.co.uk>
2017-01-29 03:58:12 +0000

    Added another heuristic

--- code/james-cpp/mcsp.c
+++ code/james-cpp/mcsp.c
@@ -23,12 +23,14 @@ static void fail(std::string msg) {
     exit(1);
 }
 
+enum Heuristic { min_max, min_product };
+
 /*******************************************************************************
                              Command-line arguments
 *******************************************************************************/
 
-static char doc[] = "Find a maximum clique in a graph in DIMACS format";
-static char args_doc[] = "FILENAME1 FILENAME2";
+static char doc[] = "Find a maximum clique in a graph in DIMACS format\vHEURISTIC can be min_max or min_product";
+static char args_doc[] = "HEURISTIC FILENAME1 FILENAME2";
 static struct argp_option options[] = {
     {"quiet", 'q', 0, 0, "Quiet output"},
     {"verbose", 'v', 0, 0, "Verbose output"},
@@ -50,6 +52,7 @@ static struct {
     bool directed;
     bool labelled;
     bool big_first;
+    Heuristic heuristic;
     char *filename1;
     char *filename2;
     int arg_num;
@@ -105,8 +108,15 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state) {
             break;
         case ARGP_KEY_ARG:
             if (arguments.arg_num == 0) {
-                arguments.filename1 = arg;
+                if (std::string(arg) == "min_max")
+                    arguments.heuristic = min_max;
+                else if (std::string(arg) == "min_product")
+                    arguments.heuristic = min_product;
+                else
+                    fail("Unknown heuristic (try min_max or min_product)");
             } else if (arguments.arg_num == 1) {
+                arguments.filename1 = arg;
+            } else if (arguments.arg_num == 2) {
                 arguments.filename2 = arg;
             } else {
                 argp_usage(state);
@@ -224,7 +234,9 @@ int select_bidomain(const vector<Bidomain>& domains, vector<int>& left,
     for (unsigned int i=0; i<domains.size(); i++) {
         const Bidomain &bd = domains[i];
         if (arguments.connected && current_matching_size>0 && !bd.is_adjacent) continue;
-        int len = std::max(bd.left_len, bd.right_len);
+        int len = arguments.heuristic == min_max ?
+                std::max(bd.left_len, bd.right_len) :
+                bd.left_len * bd.right_len;
         if (len < min_size) {
             min_size = len;
             min_tie_breaker = find_min_value(left, bd.l, bd.left_len);
