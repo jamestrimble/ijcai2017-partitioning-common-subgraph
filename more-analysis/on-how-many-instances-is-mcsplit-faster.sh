76e2bd9eecbf7df55a7b2bf8a0ebfdd5ffe090b3
James Trimble <james.trimble@yahoo.co.uk>
2017-02-17 13:26:16 +0000

    I'd forgotten to include kdown in the test of how many instance McSplit was fastest on

--- more-analysis/on-how-many-instances-is-mcsplit-faster.sh
+++ more-analysis/on-how-many-instances-is-mcsplit-faster.sh
@@ -1,4 +1,7 @@
-echo "Number of instances where at least one of clique, CP-FC, McClique didn't time out"
-cat ../experiments/gpgnode-results/mcsplain/runtimes.data | awk 'NR>1 && ($2<1000000 || $3<1000 || $8<1000000)' | wc -l
-echo "Number of these where McClique wan't strictly the fastest"
-cat ../experiments/gpgnode-results/mcsplain/runtimes.data | awk 'NR>1 && ($2<1000000 || $3<1000 || $8<1000000) && ($2<=$8 || $3*1000<=$8)' | wc -l
+echo "Number of instances where at least one of clique, CP-FC, McSplit didn't time out"
+cat ../experiments/gpgnode-results/mcsplain/runtimes.data | \
+  awk 'NR>1 && ($2<1000000 || $3<1000 || $5<1000000 || $8<1000000)' | wc -l
+
+echo "Number of these where McSplit wan't strictly the fastest"
+cat ../experiments/gpgnode-results/mcsplain/runtimes.data | \
+  awk 'NR>1 && ($2<1000000 || $3<1000 || $5<1000000 || $8<1000000) && ($2<=$8 || $3*1000<=$8 || $5<=$8)' | wc -l
